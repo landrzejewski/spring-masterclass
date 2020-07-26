4fa4933eb090dd8d5ef793d783dc213905c23ba8
landrzejewski <landrzejewski.poczta@gmail.com>
2020-07-26 13:37:05 +0200

    Angular

---
+++ shop-ui/src/app/component/users-list/users-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../service/user.service";
+import {PagedResultModel} from "../../model/paged-result.model";
+import {UserModel} from "../../model/user.model";
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.css']
+})
+export class UsersListComponent {
+
+  pagedResult: PagedResultModel<UserModel>;
+
+  constructor(private userService: UserService) {
+    this.reload(0);
+  }
+
+  private reload(pageNumber: number) {
+    this.userService.getUsers(pageNumber)
+      .subscribe(pagedResult => this.pagedResult = pagedResult, error => console.log(error));
+  }
+
+  previous() {
+    this.reload(this.pagedResult.pageNumber - 1);
+  }
+
+  next() {
+    this.reload(this.pagedResult.pageNumber + 1);
+  }
+
+}
