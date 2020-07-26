2ecdf93a8a70b729c962b198b99f0c7acc8ea18c
landrzejewski <landrzejewski.poczta@gmail.com>
2020-07-26 22:09:47 +0200

    JSP

--- src/main/java/pl/training/shop/users/UserWebController.java
+++ src/main/java/pl/training/shop/users/UserWebController.java
@@ -1,8 +1,10 @@
 package pl.training.shop.users;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,13 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 import pl.training.shop.common.PagedResult;
 
 import javax.validation.Valid;
+import java.util.Locale;
 
 @Controller
 @RequiredArgsConstructor
 public class UserWebController {
 
     private final UserService userService;
-    private final UserMapper userMapper;
+    private final MessageSource messageSource;
 
     @GetMapping("add-user.html")
     public ModelAndView addUser() {
@@ -42,6 +45,12 @@ public class UserWebController {
         ModelAndView modelAndView = new ModelAndView("users");
         modelAndView.addObject("users", users);
         return modelAndView;
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ModelAndView onBankException(UserNotFoundException ex, Locale locale) {
+        String message = messageSource.getMessage(ex.getClass().getSimpleName(), null, locale);
+        return new ModelAndView("error", "message", message);
     }
 
 }
