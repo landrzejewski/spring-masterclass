eda09ce118bce83f93ff1cb5d92166d1e1e194bb
landrzejewski <landrzejewski.poczta@gmail.com>
2020-07-28 15:57:28 +0200

    Products

---
+++ src/main/java/pl/training/shop/products/ProductRestController.java
@@ -0,0 +1,43 @@
+package pl.training.shop.products;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.training.shop.common.PagedResult;
+import pl.training.shop.common.web.PagedResultTransferObject;
+import pl.training.shop.common.web.UriBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RequestMapping("api/products")
+@RestController
+@RequiredArgsConstructor
+public class ProductRestController {
+
+    private final ProductService productService;
+    private final ProductMapper productMapper;
+
+    private final UriBuilder uriBuilder = new UriBuilder();
+
+    @PostMapping
+    public ResponseEntity<ProductTransferObject> addProduct(@Valid @RequestBody ProductTransferObject productTransferObject, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+        Product product = productMapper.toProduct(productTransferObject);
+        Long productId = productService.add(product).getId();
+        URI locationUri = uriBuilder.requestUriWithId(productId);
+        return ResponseEntity.created(locationUri).build();
+    }
+
+    @GetMapping
+    public PagedResultTransferObject<ProductTransferObject> getProducts(
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam(defaultValue = "5") int pageSize) {
+        PagedResult<Product> products = productService.getAll(pageNumber, pageSize);
+        return productMapper.toProductTransferObjectsPage(products);
+    }
+
+}
