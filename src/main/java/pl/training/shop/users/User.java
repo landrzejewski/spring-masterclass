a2f24142852b9ee70a6acd806afa82a5e277306f
landrzejewski <landrzejewski.poczta@gmail.com>
2020-09-30 22:20:45 +0200

    BaseEntity

--- src/main/java/pl/training/shop/users/User.java
+++ src/main/java/pl/training/shop/users/User.java
@@ -1,6 +1,7 @@
 package pl.training.shop.users;
 
 import lombok.*;
+import pl.training.shop.common.BaseEntity;
 import pl.training.shop.common.validator.Name;
 
 import javax.persistence.*;
@@ -9,15 +10,13 @@ import javax.validation.constraints.Pattern;
 @Table(name = "users", indexes = @Index(name = "email", columnList = "email"))
 @Entity
 @Builder
-@EqualsAndHashCode(exclude = "id")
-@Data
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User extends BaseEntity {
 
-    @GeneratedValue
-    @Id
-    private Long id;
     @Pattern(regexp = "[A-Za-z]+")
     @Column(name = "first_name", nullable = false)
     private String firstName;
