6822c475c9b2c2af063dd51bd1038ecffb7a829f
landrzejewski <landrzejewski.poczta@gmail.com>
2020-09-30 21:44:48 +0200

    BaseEntity

--- src/main/java/pl/training/shop/payments/Payment.java
+++ src/main/java/pl/training/shop/payments/Payment.java
@@ -4,17 +4,20 @@ import lombok.*;
 import org.hibernate.annotations.Columns;
 import org.hibernate.annotations.TypeDef;
 import org.javamoney.moneta.FastMoney;
+import pl.training.shop.common.BaseEntity;
 import pl.training.shop.common.FastMoneyUserType;
 
 import javax.persistence.*;
 import java.time.Instant;
+import java.util.Objects;
 
 @TypeDef(name = "fastMoney", typeClass = FastMoneyUserType.class, defaultForType = FastMoney.class)
 @Table(name = "payments", indexes = @Index(name = "payment_status", columnList = "status"))
 @Entity
 @Builder
-@EqualsAndHashCode(exclude = "id")
-@Data
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class Payment {
@@ -30,5 +33,23 @@ public class Payment {
     private Instant timestamp;
     @Enumerated(EnumType.STRING)
     private PaymentStatus status;
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (this == otherObject) {
+            return true;
+        }
+        if (getClass() != otherObject.getClass()) {
+            return false;
+        }
+        Payment otherEntity = (Payment) otherObject;
+        return this.id != null && Objects.equals(id, otherEntity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
 
 }
--- src/main/java/pl/training/shop/users/User.java
+++ src/main/java/pl/training/shop/users/User.java
@@ -1,21 +1,20 @@
 package pl.training.shop.users;
 
 import lombok.*;
+import pl.training.shop.common.BaseEntity;
 
 import javax.persistence.*;
 
 @Table(name = "users", indexes = @Index(name = "email", columnList = "email"))
 @Entity
 @Builder
-@EqualsAndHashCode(exclude = "id")
-@Data
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User extends BaseEntity {
 
-    @GeneratedValue
-    @Id
-    private Long id;
     @Column(name = "first_name", nullable = false)
     private String firstName;
     @Column(name = "last_name", nullable = false)
